7bb7c3cc4cdd3bc113d12efad4f7ed184c1415ca
Sameer <103096691+SameerKapse@users.noreply.github.com>
2023-08-06 12:40:29 +0530

    Add files via upload

---
+++ Patterns/pattern6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 5; i++)
+    {
+         
+        for (int j = 1; j <=5-i+1; j++)
+        {
+            cout<<j;
+        }
+        
+         cout<<endl;
+    }
+    
+    return 0;
+}
+/*
+
+Output: 
+12345
+1234
+123
+12
+1
+
+*/
